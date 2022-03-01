96b08922a78c63dee2489f97a1c22189d334ea97
tinyriggs <nr107984@gmail.com>
2022-02-28 19:18:53 -0500

    letter check
    started letter check function

--- WordleHelper.py
+++ WordleHelper.py
@@ -1,13 +1,15 @@
 
 words = []
 answer = {}
-
+acab = []
+letters = ["s", "e", "a", "o", "r"]
 
 
 with open("C:/Users/nr107/Desktop/WordleProgram/words.txt.txt") as f:
     words = f.readlines()
 
 for word in words:
+    word = word.rstrip()
     for letter in word:
         if letter in answer:
             answer[letter] += 1
@@ -17,13 +19,19 @@ for word in words:
 sorted_answer = sorted(answer.items(), key=lambda x: x[1], reverse = True)
 
 
-for i in sorted_answer:
-    print(i[0], i[1])
+# for i in sorted_answer:
+#     print(i[0], i[1])
 
+def letter_check(word, letter, element):
+    if word[element] == letter:
+        return True
+    else:
+        return False
 
 for word in words:
-    for letter in word:
-        if letter != "s":
-            break
-        else:
-            print(word)
+    word = word.rstrip()
+    fuck = letter_check(word,"s",0)
+
+
+
+print(acab)
