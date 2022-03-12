803a6dee31b361d735d54c2b105f1e3e9192ff68
tinyriggs <nr107984@gmail.com>
2022-03-12 17:33:51 -0500

    Updated
    Updated to not have a bunch of holder arrays. Also updated some variable names to be clearer

--- WordleHelper.py
+++ WordleHelper.py
@@ -1,12 +1,10 @@
+import string
+
 
 words = []
-answer = {}
-first_letters = []
-second_letters = []
-third_letters = []
-fourth_letters = []
-fifth_letters = []
-letters = []
+stripped_words = []
+letter_quantities = {}
+top_5_letters = []
 
 
 with open("C:/Users/nr107/Desktop/WordleProgram/words.txt") as f:
@@ -15,17 +13,17 @@ with open("C:/Users/nr107/Desktop/WordleProgram/words.txt") as f:
 for word in words:
     word = word.rstrip()
     for letter in word:
-        if letter in answer:
-            answer[letter] += 1
+        if letter in letter_quantities:
+            letter_quantities[letter] += 1
         else:
-            answer[letter] = 1
+            letter_quantities[letter] = 1
 
-sorted_answer = dict(sorted(answer.items(), key=lambda x: x[1], reverse = True))
+sorted_answer = dict(sorted(letter_quantities.items(), key=lambda x: x[1], reverse = True))
 
 counter = 0
 for key in sorted_answer.keys():
     if counter < 5:
-        letters.append(key)
+        top_5_letters.append(key)
         counter += 1
 
 
@@ -35,35 +33,13 @@ def letter_check(word, letter, element):
     else:
         return False
 
+def word_check(word, letter_element):
+    for letter in top_5_letters:
+        if letter_check(word, letter, letter_element):
+            return True
+    return False
 
-for word in words:
-    word = word.rstrip()
-    for letter in letters:
-        if letter_check(word, letter, 0):
-            first_letters.append(word)
-
-for word in first_letters:
-    for letter in letters:
-        if letter_check(word, letter, 1):
-            second_letters.append(word)
-
-for word in second_letters:
-    for letter in letters:
-        if letter_check(word, letter, 2):
-            third_letters.append(word)
-
-for word in third_letters:
-    for letter in letters:
-        if letter_check(word, letter, 3):
-            fourth_letters.append(word)
-
-for word in fourth_letters:
-    for letter in letters:
-        if letter_check(word, letter, 4):
-            fifth_letters.append(word)
-
-
-def are_letters_matching(word):
+def are_letters_different(word):
     counter = 4
     for num in range(5):
         for x in range(counter):
@@ -73,6 +49,29 @@ def are_letters_matching(word):
             return True
         counter -= 1
 
-for word in fifth_letters:
-    if are_letters_matching(word):
-        print(word)
+
+def find_optimal_words(words_to_check):
+    holder = []
+    for word in words_to_check:
+        for element in range(0,5):
+            if word_check(word, element) == False:
+                break
+            elif element == 4:
+                holder.append(word)
+    letter_quantities = []
+    for word in holder:
+        if are_letters_different(word):
+            letter_quantities.append(word)
+    return letter_quantities
+
+def strip_words(words_to_strip):
+    letter_quantities = []
+    for word in words_to_strip:
+        letter_quantities.append(word.rstrip())
+    return letter_quantities
+
+stripped_words = strip_words(words)
+
+optimal_words = find_optimal_words(stripped_words)
+
+print(optimal_words)
